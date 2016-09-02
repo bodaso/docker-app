bea8b67b3837aa9c7445080466b2544da8c35faf
Boda <bodazhao@hotmail.com>
2016-09-02 16:24:43 +0100

    changed to mysql pool and fixed connection bug

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,20 +1,20 @@
 var express = require('express');
 var mysql = require('mysql');
-
 var router = express.Router();
 
-var connection = mysql.createConnection({
+var mPool = mysql.createPool({
   host: process.env.DATABASE_HOST || 'localhost',
   port: 3306,
   database: 'users',
   user: 'root_user',
-  password: 'abcd123'
+  password: 'abcd123',
+  connectionLimit: 100
 });
 
 /* GET home page. */
 router.get('/', function(req, res) {
 
-  connection.connect(function(err){
+  mPool.getConnection(function(err, connection){
     if(!err) {
       console.log("Database is connected ... nn");
     } else {
@@ -24,9 +24,9 @@ router.get('/', function(req, res) {
     connection.query('SELECT count(*) AS total_users FROM directory', function(err, rows) {
       if (err) throw err;
       res.render('index', { title: 'index page', counter: rows[0].total_users });
-      connection.end();
     });
 
+    connection.release();
   });
 
 });
